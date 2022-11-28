26803ff34a2de4dba9df9b2638e0a50783176a7b
emmaduarte <emma.ale.duarte@gmail.com>
2022-11-28 01:35:18 -0300

    Arreglos de route

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     
       
-      <BrowserRouter basename='/Portfolio-React'>
+      <BrowserRouter>
         <Nav/>
         <Routes>
           <Route path='/Portfolio-React' exact element={<Header/>}/>
