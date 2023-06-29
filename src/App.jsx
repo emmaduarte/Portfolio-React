a09f5eec1c43cff2b2717ad3261d24c190c9229b
emmaduarte <emma.ale.duarte@gmail.com>
2023-06-29 00:04:43 -0300

    Quitando router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import React from 'react'
-import Header from './componentes/header/Header'
-import Nav from './componentes/nav/Nav'
-import SobreMi from './componentes/sobreMi/SobreMi'
-import Experiencia from './componentes/experiencia/Experiencia'
-import Porfolio from './componentes/portfolio/Portfolio'
-import Contacto from './componentes/contacto/Contacto'
-import Footer from './componentes/footer/Footer'
-import {BrowserRouter, Router, Route, Routes, HashRouter} from "react-router-dom"
+import React from "react";
+import Header from "./componentes/header/Header";
+import Nav from "./componentes/nav/Nav";
+import SobreMi from "./componentes/sobreMi/SobreMi";
+import Experiencia from "./componentes/experiencia/Experiencia";
+import Porfolio from "./componentes/portfolio/Portfolio";
+import Contacto from "./componentes/contacto/Contacto";
+import Footer from "./componentes/footer/Footer";
+import {
+  BrowserRouter,
+  Router,
+  Route,
+  Routes,
+  HashRouter,
+} from "react-router-dom";
 
 const App = () => {
   return (
-    
-      
-      <>
-      <BrowserRouter>
-        <Nav/>
+    <>
+      <Header />
+      <SobreMi />
+      <Experiencia />
+      <Porfolio />
+      <Contacto />
+      {/* <BrowserRouter>
+        <Nav />
         <Routes>
-          <Route path='/' element={<Header/>}/>
-          <Route path='/SobreMi' exact element={<SobreMi/>}/>
-          <Route path='/Experiencia' exact element={<Experiencia/>}/>
-          <Route path='/Portfolio' exact element={<Porfolio/>}/>
-          <Route path='/Contacto' exact element={<Contacto/>}/>
-
+          <Route path="/" element={<Header />} />
+          <Route path="/SobreMi" exact element={<SobreMi />} />
+          <Route path="/Experiencia" exact element={<Experiencia />} />
+          <Route path="/Portfolio" exact element={<Porfolio />} />
+          <Route path="/Contacto" exact element={<Contacto />} />
         </Routes>
-        <Footer/>
-      </BrowserRouter>
-      </>
+        <Footer />
+      </BrowserRouter> */}
+    </>
+  );
+};
 
-  
-  )
-}
-
-export default App
+export default App;
